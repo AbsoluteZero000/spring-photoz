06a68baf4b384d6511691cea47fdac4efa7ada24
Ahmed Wael Wanas <ahmedwaelwanas@gmail.com>
2026-01-21 11:14:31 +0200

    initial commit

---
+++ src/main/java/com/zero/springphotoz/SpringPhotozApplication.java
@@ -0,0 +1,13 @@
+package com.zero.springphotoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPhotozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPhotozApplication.class, args);
+    }
+
+}
